2401b587ed4b0c04a0c47e7b13ea68832940b2e7
Pat Barber <patbarber72@gmail.com>
2023-05-02 09:17:48 +1000

    made navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <div className="border-2 border-black m-2 grid grid-cols-2">
-      <Link to="/">Home</Link>
-      <div className="grid grid-cols-4 justify-items-center">
+    <div className="border-2 border-black m-2 grid grid-cols-1 md:grid-cols-2">
+      <Link className="justify-self-center md:justify-self-start" to="/">
+        Home
+      </Link>
+      <div className="grid grid-cols-1 md:grid-cols-4 justify-items-center">
         <div>Notifications</div>
         <Link to="/orders">Orders</Link>
         <div>Other</div>
