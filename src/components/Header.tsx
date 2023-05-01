60cad29d40cf637fd2a8d7f9817555c80acea23f
Pat Barber <patbarber72@gmail.com>
2023-05-02 09:15:04 +1000

    changed header to be responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
 export const Header = () => {
   return (
     <div className="w-full h-full inset-x-0 top-0 border-b-2">
-      <div className="grid grid-cols-3">
+      <div className="md:hidden">
+        <div className="mx-auto w-fit">logo</div>
+      </div>
+      <div className="hidden md:grid md:grid-cols-3">
         <div className="justify-self-start">logo</div>
         <div className="justify-self-center">Title</div>
         <div className="justify-self-end">Log in </div>
