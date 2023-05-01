32263632cf8326e4ef73878c9b1974f86d8e739a
Pat Barber <patbarber72@gmail.com>
2023-05-01 16:17:38 +1000

    added routes; home, order pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Navbar } from "./components/Navbar";
-
+import { Home } from "./pages/Home";
+import { Order } from "./pages/Order";
 function App() {
   return (
     <>
       <Header />
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/orders" element={<Order />} />
+      </Routes>
       <Footer />
     </>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 export const Navbar = () => {
   return (
     <div className="border-2 border-black m-2 grid grid-cols-2">
-      <div>Home</div>
+      <Link to="/">Home</Link>
       <div className="grid grid-cols-4 justify-items-center">
         <div>Notifications</div>
-        <div>Orders</div>
+        <Link to="/orders">Orders</Link>
         <div>Other</div>
         <div>Help</div>
       </div>
