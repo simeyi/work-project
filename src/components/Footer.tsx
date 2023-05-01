939c01545fdc8b7384a1d525a58358b6e0727db4
Pat Barber <patbarber72@gmail.com>
2023-05-02 09:50:25 +1000

    made footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <div className="absolute inset-x-0 bottom-0 border-t-2">
-      <div className="m-2 border-2 grid grid-cols-5 justify-items-center">
+      <div className="m-2 border-2 grid grid-cols-1 md:grid-cols-5 justify-items-center">
         <div>About</div>
         <div>Copyright</div>
         <div>Disclaimer</div>
