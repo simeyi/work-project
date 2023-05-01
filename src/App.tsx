5712265650e28ec3d9267de5ed42b502107cf702
Pat Barber <patbarber72@gmail.com>
2023-05-01 11:37:52 +1000

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Navbar } from "./components/Navbar";
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Navbar />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+export const Footer = () => {
+  return (
+    <div className="absolute inset-x-0 bottom-0 border-t-2">
+      <div className="m-2 border-2 grid grid-cols-5 justify-items-center">
+        <div>About</div>
+        <div>Copyright</div>
+        <div>Disclaimer</div>
+        <div>Privacy Notice</div>
+        <div>Contact</div>
+      </div>
+    </div>
+  );
+};
