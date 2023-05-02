3505f3781da93d8595aa9a7a82ceb868532f7c24
Pat Barber <patbarber72@gmail.com>
2023-05-02 16:20:59 +1000

    added maintenance flag/view and types

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,30 @@ import { Notifications } from "./pages/Notifications";
 import { Order } from "./pages/Order";
 import { Other } from "./pages/Other";
 function App() {
+  const maintenanceMode = import.meta.env.VITE_MAINTENANCEMODE;
+
   return (
     <>
       <Header />
-      <Navbar />
-      <div className="m-2 border-2 shadow-md">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/notifications" element={<Notifications />} />
-          <Route path="/orders" element={<Order />} />
-          <Route path="/other" element={<Other />} />
-          <Route path="/help" element={<Help />} />
-        </Routes>
-      </div>
+      {maintenanceMode === "false" && (
+        <>
+          <Navbar />
+          <div className="m-2 border-2 shadow-md">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/notifications" element={<Notifications />} />
+              <Route path="/orders" element={<Order />} />
+              <Route path="/other" element={<Other />} />
+              <Route path="/help" element={<Help />} />
+            </Routes>
+          </div>
+        </>
+      )}
+      {maintenanceMode === "true" && (
+        <div className="m-2 border-2 shadow-md">
+          <p>currently in maintenance Mode</p>
+        </div>
+      )}
       <Footer />
     </>
   );
