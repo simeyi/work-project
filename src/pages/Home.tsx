ff47bd2c8ee4290623c4e743523b20ff2d66fc3e
DenySim <sim_deny81@yahoo.com>
2023-05-03 13:45:54 +1000

    Add inputField Styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
 import { TextInput } from "../components/form/textInput";
 
 export const Home = () => {
-  return( <div><>Home</><br/><br/>
-    <TextInput label="Name of the Request" placeholder ="Enter the name" error="false"/></div>
+  return (
+    <div>
+      <>Home</>
+
+      <TextInput
+        label="Name of the Request"
+        placeholder="Enter the name"
+        error={true}
+      />
+    </div>
   );
 };
