83033fda20513ab635952125e82c382c60eca73c
DenySim <sim_deny81@yahoo.com>
2023-05-03 13:12:10 +1000

    updateInputField

--- src/components/form/textInput.tsx
+++ src/components/form/textInput.tsx
@@ -1,8 +1,27 @@
-export const textInput = () => {
+export const TextInput = (props:any) => {
+  const { label, placeholder, error } = props;
   return (
     <>
-      <label>test</label>
-      <input type="text" />
+      {error === "false" && ( 
+      <>
+        <div className="sm:col-span-4">
+          <label className="block text-sm font-medium leading-6 text-gray-900">{label}</label>
+          <div className="mt-2">
+            <input type="text" placeholder={placeholder} className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+        </div>
+      </>
+      )}
+      {error === "true" && ( 
+      <>
+        <div className="sm:col-span-4">
+          <label className="block text-sm font-medium leading-6 text-gray-900">{label}</label>
+          <div className="mt-2">
+            <input type="text" placeholder={placeholder} className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring ring-inset ring-red-500 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+        </div>
+      </>
+      )}
     </>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,7 @@
+import { TextInput } from "../components/form/textInput";
+
 export const Home = () => {
-  return <>Home</>;
+  return( <div><>Home</><br/><br/>
+    <TextInput label="Name of the Request" placeholder ="Enter the name" error="false"/></div>
+  );
 };
