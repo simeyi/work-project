ead4e931905e80711d09eac09aae015bf61075a3
DenySim <sim_deny81@yahoo.com>
2023-05-04 08:30:15 +1000

    update fileUpload multiple

--- src/components/form/fileUpload.tsx
+++ src/components/form/fileUpload.tsx
@@ -3,7 +3,7 @@ export const FileUpload = ({ label, fileType, fileMax }: any) => {
     <>
       <label className="block text-gray-900">{label}</label>
       <label className="text-center mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10 relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
-        <input id="file-upload" name="file-upload" type="file" />
+        <input id="file-upload" name="file-upload" type="file" multiple />
         <span className="pl-3 text-xs leading-7 text-gray-600">
           {fileType} up to {fileMax}
         </span>
