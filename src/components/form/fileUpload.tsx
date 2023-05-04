d55217da454f41f2cae8bd23d33b077007d36640
DenySim <sim_deny81@yahoo.com>
2023-05-04 13:55:56 +1000

    update FileUpload list and delete

--- src/components/form/fileUpload.tsx
+++ src/components/form/fileUpload.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export const FileUpload = ({ label, fileType, fileMax }: any) => {
   const [selectedFile, setSelectedFile] = useState<any[]>([]);
