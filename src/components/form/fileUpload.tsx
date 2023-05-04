61f83bf2da7396c7f02dd4767cbde6a710fcef65
DenySim <sim_deny81@yahoo.com>
2023-05-04 14:45:45 +1000

    updat FileUpload change table to grid

--- src/components/form/fileUpload.tsx
+++ src/components/form/fileUpload.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { TrashIcon } from "@radix-ui/react-icons";
 
 export const FileUpload = ({ label, fileType, fileMax }: any) => {
   const [selectedFile, setSelectedFile] = useState<any[]>([]);
@@ -69,29 +70,22 @@ export const FileUpload = ({ label, fileType, fileMax }: any) => {
         </div>
       </div>
 
-      <table className="min-w-full divide-y divide-gray-200">
-        <tbody className="divide-y divide-gray-200">
-          {files.map((file: any) => (
-            <tr key={file.name}>
-              <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                {file.name}
-              </td>
-              <td>
-                <svg
-                  onClick={() => onDeleteFile(file)}
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  className="w-4 h-4 cursor-pointer"
-                >
-                  <path d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                </svg>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="grid grid-cols-2">
+        {files.map((file: any) => (
+          <>
+            <div className="px-4 py-2 text-sm text-gray-800 ">{file.name}</div>
+            <div>
+              <button
+                type="button"
+                className="px-4 py-2 float-right"
+                onClick={() => onDeleteFile(file)}
+              >
+                <TrashIcon />
+              </button>
+            </div>
+          </>
+        ))}
+      </div>
 
       <button
         onClick={printFileDetails}
