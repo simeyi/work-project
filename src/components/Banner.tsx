9e5f6d45ef76e988b8d9bfc9caef80636a7d55a8
DenySim <sim_deny81@yahoo.com>
2023-05-08 09:38:38 +1000

    add feature/splash-banner initial

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,8 +5,13 @@ export const Banner = () => {
         Welcome to the My Portal
       </h1>
       <p className="mt-2 text-xl text-gray-500">
-        This year, our new summer collection will shelter you from the harsh
-        elements of a world that doesn't care if you live or die.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
       </p>
     </div>
   );
