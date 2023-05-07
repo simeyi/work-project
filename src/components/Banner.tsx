a1942d9deaf127e222a7a802125ce38f529162d7
DenySim <sim_deny81@yahoo.com>
2023-05-08 09:37:10 +1000

    add feature/splash-banner initial

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,7 +2,7 @@ export const Banner = () => {
   return (
     <div className="sm:max-w-6xl">
       <h1 className="font text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-        Welcome to the Estate Exchange Portal
+        Welcome to the My Portal
       </h1>
       <p className="mt-2 text-xl text-gray-500">
         This year, our new summer collection will shelter you from the harsh
