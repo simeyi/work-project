c5777568199967e6ef41537127c97a915161000a
Pat Barber <patbarber72@gmail.com>
2023-05-08 10:29:01 +1000

    link card outline

--- src/components/LinkCard.tsx
+++ src/components/LinkCard.tsx
@@ -1,7 +1,24 @@
-export const LinkCard = () => {
+import { Link } from "react-router-dom";
+
+export const LinkCard = ({
+  label,
+  link,
+  description,
+}: {
+  label: string;
+  link: string;
+  description: string;
+}) => {
   return (
     <>
-      <div>link card </div>
+      <Link to={link}>
+        <div className="aspect-square border-2 w-48 md:w-48 p-2">
+          <div className="border-2 w-4/5 px-2 py-1">{label} </div>
+          <div className="p-1 overflow-hidden text-ellipsis line-clamp-5">
+            {description}
+          </div>
+        </div>
+      </Link>
     </>
   );
 };
